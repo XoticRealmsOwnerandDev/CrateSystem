0edd62c181dc536a71aca9c67937e0e5cae89795
ArmTheDev <armagadon159753@gmail.com>
2017-12-31 01:38:56 +0100

    Update Common.php

--- src/Crates/Common.php
+++ src/Crates/Common.php
@@ -50,7 +50,7 @@ class Common{
             $inv->removeItem(Item::get(131,1,1));
             $sender->addTitle("§eOpening Crate:", "§aCommon!");
             $this->plugin->getServer()->broadcastMessage("§b$name §ajust opened §aCommon §aCrate!");
-            $result = rand(1,1);
+            $result = mt_rand(1,1);
                  switch($result){
         case 1:
         $sender->getInventory()->addItem(Item::get($values[0], $values[1], $values[2])->setCustomName($values[3]));
